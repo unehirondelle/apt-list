0bf579ed073a1d0798cf1cdd7fd9e80df6e9d97e
Irina Plaksina <irene.plaksina@gmail.com>
2020-09-13 20:18:32 -0400

    Updates

---
+++ precache-manifest.847d72854c9ee3021e2687696e8957e4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55be1956e20287b8fac35ccf6dea08ac",
+    "url": "/apt-list/index.html"
+  },
+  {
+    "revision": "05cc2bee6d9940875370",
+    "url": "/apt-list/static/css/2.6ee32efb.chunk.css"
+  },
+  {
+    "revision": "7e1aee04a113cb533ca0",
+    "url": "/apt-list/static/css/main.b2f8daa9.chunk.css"
+  },
+  {
+    "revision": "05cc2bee6d9940875370",
+    "url": "/apt-list/static/js/2.b55f75c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apt-list/static/js/2.b55f75c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1aee04a113cb533ca0",
+    "url": "/apt-list/static/js/main.23d07ec5.chunk.js"
+  },
+  {
+    "revision": "aa53571a350b8535ddee",
+    "url": "/apt-list/static/js/runtime-main.4022146b.js"
+  },
+  {
+    "revision": "cb331afa877ee4c28cba8cc69c5e2b52",
+    "url": "/apt-list/static/media/apartment.cb331afa.jpg"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/apt-list/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/apt-list/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "5e99edcb80abc73c7a06aacfddca51bf",
+    "url": "/apt-list/static/media/boxicons.5e99edcb.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/apt-list/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/apt-list/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "cfecc33f95db0a7e2ccc76713ad61de0",
+    "url": "/apt-list/static/media/jumbotron-img.cfecc33f.jpg"
+  }
+]);
